95e5fea3b55f4c893574642baa1adabaaa01d165
Nikola1998 <109683915+Nikola1998@users.noreply.github.com>
2022-09-19 23:48:53 +0200

    Add files via upload

---
+++ src/bullscows/RandomNumberGenerator.java
@@ -0,0 +1,30 @@
+package bullscows;
+
+import java.util.Random;
+
+public class RandomNumberGenerator {
+    private final Random random = new Random();
+
+    public String generateNumber(int length, int possibleCharacters) {
+        StringBuilder result = new StringBuilder();
+        int randomNumber;
+        for (int i = 0; i < length; i++) {
+            while (true) {
+                if (possibleCharacters <= 10) {
+                    randomNumber = random.nextInt( possibleCharacters) + 48;
+                }
+                else {
+                    if (random.nextBoolean()) {
+                        randomNumber = random.nextInt(10) + 48;
+                    } else {
+                        randomNumber = random.nextInt(possibleCharacters - 11) + 97;
+                    }
+                }
+                if (result.indexOf("" + (char)randomNumber) == -1) {
+                    result.append((char)randomNumber);
+                    break;
+                }
+            }
+        } return result.toString();
+    }
+}
